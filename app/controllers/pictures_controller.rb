2322803ff99407d4e75f305008471a597e791a30
Sep Bar <sepand.barkhodaee@gmail.com>
2014-01-27 17:22:05 -0500

    Showing pictures at /pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,20 @@
 class PicturesController < ApplicationController
 
 	def index
+		@pictures = [
+			{
+				:title => "Gears",
+				:artist => "mech site",
+				:url => "http://www.daerospace.com/MechanicalSystems/GearsGenFigure%201.png"
+			},
+
+			{
+				:title => "IC engine",
+				:artist => "thermo site",
+				:url => "http://www.mae.wvu.edu/~smirnov/mae320/figs/F9-1.jpg"
+			}
+
+		]
 	end
 
 end
