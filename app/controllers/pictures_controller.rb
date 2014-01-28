cbd98514d0c2f26ed4bb7f17baccc43e0a444e69
Sep Bar <sepand.barkhodaee@gmail.com>
2014-01-28 15:12:56 -0500

    Making each pic show at URL using static routes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -16,5 +16,21 @@ class PicturesController < ApplicationController
 
 		]
 	end
+	def picture0
+		@picture = {
+			:title => "IC engine",
+			:artist => "thermo site",
+			:url => "http://www.mae.wvu.edu/~smirnov/mae320/figs/F9-1.jpg"
 
+			}
+	end
+
+	def picture1
+		@picture = {
+
+			:title => "Gears",
+			:artist => "mech site",
+			:url => "http://www.daerospace.com/MechanicalSystems/GearsGenFigure%201.png"
+			}
+	end
 end
